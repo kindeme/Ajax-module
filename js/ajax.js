35f662c1c8d2af1690990ad6b1cdb68ec7c6992a
Kindeme <fkounoudji@gmail.com>
2020-02-22 23:00:20 -0600

    new ajax

--- js/ajax.js
+++ js/ajax.js
@@ -2,18 +2,28 @@ const btn =document.querySelector('.btn');
 const info =document.querySelector('.info');
 // 
 
-
 function getData(url){
+
   const ajax = new XMLHttpRequest();
+
 ajax.open('GET','text.txt',true);
-ajax.onreadystatechange = function(){
-  if(this.status === 200 && this.readyState ===4){
+// ajax.onreadystatechange = function(){
+//   if(this.status === 200 && this.readyState ===4){
+//     info.textContent = this.responseText;
+//   }
+// };
+ajax.onload = function(){
+  if(this.status ==200){
     info.textContent = this.responseText;
   }
-};
+}
+//  when it is error
+ajax.onerror = function(){
+  console.log('there was an error');
+}
+
 ajax.send();
 }
 btn.addEventListener("click",function(){
-  getData("name.txt");
-
+  getData("text.txt");
 })
