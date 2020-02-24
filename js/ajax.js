95d327f9f07a8cd6c7a6f11d023eaff80c8e86cc
Kindeme <fkounoudji@gmail.com>
2020-02-24 15:00:49 -0600

    new ajax

--- js/ajax.js
+++ js/ajax.js
@@ -12,28 +12,27 @@ function getData(url){
   const ajax = new XMLHttpRequest();
  ajax.open('GET',url,true);
 
-
 ajax.onload = function(){
   if(this.status ==200){
    const data = JSON.parse(this.responseText);
    cartInfo.innerHTML = `<p> ${data.cartInfo.name}</p>
                         <p> ${data.cartInfo.store}</p>`;
-  let displa
 
+   // filter example
+   let display = ' ' ;
+   const specialItems = data.cartItems.filter(function(item){
+    return item.price >1;
+  })
 
+specialItems.cartItems.forEach(function(item){
+  display += `<div class="item> 
+  <p>item id:${item.id}</p>
+  <p>item name:${item.name}</p>
+  <p>item price:${item.price}</p>
+  </div> `;
+  itemInfo.innerHTML = display;
 
-
-
-
-
-
-
-
-
-
-
-
-    
+});
   }else{
     this.onerror();
   }
