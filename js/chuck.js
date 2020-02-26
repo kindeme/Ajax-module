ceb08fadab8a5f5371832bd9d246fbf542eca500
Kindeme <fkounoudji@gmail.com>
2020-02-25 23:31:44 -0600

    random joke

--- js/chuck.js
+++ js/chuck.js
@@ -1,3 +1,22 @@
 const mainbtn = document.getElementById('mainBtn');
 const result = document.getElementById("result");
 const img = document.getElementById('img');
+
+mainbtn.addEventListener("click", function(){
+  const ajax = new XMLHttpRequest();
+  const url = 'https://api.chucknorris.io/jokes/random';
+  ajax.open("GET",url,true);
+  ajax.onload = function(){
+    if(this.status == 200){
+      const data = JSON.parse(this.responseText);
+      const {icon_url:image, value:joke} = data;
+      result.textContent = joke;
+      img.src = image;
+    }
+  };
+  ajax.onerror = function(){
+    console.log("there was an error");
+  };
+  ajax.send();
+
+});
