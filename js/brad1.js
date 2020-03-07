ef077f7e65fc1eac23d579cbd85622370f9a40de
Kindeme <fkounoudji@gmail.com>
2020-03-06 19:23:04 -0600

    Api

---
+++ js/brad1.js
@@ -0,0 +1,19 @@
+// const post =[
+//   {title: 'Post One',body:"This is post one"},
+//   {title: 'Post two',body:"This is post two"}
+
+// ];
+// function createPost(){
+//   setTimeout(function(){
+//     setTimeout(function(){
+//       posts.push(post);
+//     })
+//   })
+
+// }
+// function getPost(){
+
+// }
+function add (x, y){
+  return x + y;
+}
