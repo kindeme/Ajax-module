db3292dd028b24d6c65e66e0dca7fdd07092b45b
Kindeme <fkounoudji@gmail.com>
2020-03-09 22:54:31 -0500

    ajax

--- js/bjosn.js
+++ js/bjosn.js
@@ -1,4 +1,5 @@
 document.getElementById('button1').addEventListener("click",loadCustomer);
+document.getElementById('button2').addEventListener("click",loadCustomers);
 
 function loadCustomer(e){
   const xhr = new XMLHttpRequest();
@@ -18,4 +19,28 @@ function loadCustomer(e){
     
   }
   xhr.send();
+}
+
+// load Customer2
+function loadCustomers(e){
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET","customers.json",true);
+
+  xhr.onload = function(){
+    if(this.status ===200){
+    const customers = JSON.parse(this.responseText);
+    let output = '';
+customers.forEach(function(customer){
+   output += `<ul> 
+  <li> ID: ${customer.id}</li>
+  <li> Name: ${customer.name}</li>
+  <li> Company: ${customer.company}</li>
+  <li> Phone: ${customer.phone}</li>
+  ` ;
+});
+  document.getElementById('customer').innerHTML = output;
+}
+}
+  xhr.send();
+
 }
