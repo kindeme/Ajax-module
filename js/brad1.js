73bb9b592b6a1cfbb2baa95df6f9ecfaafb255ee
Kindeme <fkounoudji@gmail.com>
2020-03-09 21:39:23 -0500

    ajax

--- js/brad1.js
+++ js/brad1.js
@@ -8,7 +8,7 @@ function loadData(){
 
   xhr.onload = function(){
     if(this.status === 200){
-      console.log(this.responseText);
+      document.getElementById('output').innerHTML =`<h1>${this.responseText}</h1>`;
     }
   }
   xhr.send();
