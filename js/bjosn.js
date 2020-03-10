39adf4e52bfee13dde73bb0f41d570c086f2440c
Kindeme <fkounoudji@gmail.com>
2020-03-09 22:24:18 -0500

    ajax

---
+++ js/bjosn.js
@@ -0,0 +1,11 @@
+document.getElementById('button1').addEventListener("click",loadCustomer);
+function loadCustomer(e){
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET","customer.json",true);
+  xhr.onload = function(){
+    if(yhis.status ===200){
+      console.log(this.responseText);
+    }
+  }
+  xhr.send();
+}
