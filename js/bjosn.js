58166ccee0316a0e71bd5f7295d10339eae84f82
Kindeme <fkounoudji@gmail.com>
2020-03-09 22:38:53 -0500

    ajax

--- js/bjosn.js
+++ js/bjosn.js
@@ -1,11 +1,21 @@
 document.getElementById('button1').addEventListener("click",loadCustomer);
+
 function loadCustomer(e){
   const xhr = new XMLHttpRequest();
   xhr.open("GET","customer.json",true);
+
   xhr.onload = function(){
-    if(yhis.status ===200){
-      console.log(this.responseText);
+    if(this.status ===200){
+    const customer = JSON.parse(this.responseText);
+    const output = `<ul> 
+    <li> ID: ${customer.id}</li>
+    <li> Name: ${customer.name}</li>
+    <li> Company: ${customer.company}</li>
+    <li> Phone: ${customer.phone}</li>
+    ` ;
+    document.getElementById('customer').innerHTML = output;
     }
+    
   }
   xhr.send();
 }
